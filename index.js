9a014f88f9aa6fc947eaf6fa494f119cb7836d79
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-05 11:51:20 +0200

    added custom middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express"
-import path from "path";
+import path from "path"
+import { requestTime, logger } from "./middlewares.js"
 
 const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
 app.use(express.static(path.resolve(__dirname,'static')))
+app.use(requestTime)
+app.use(logger)
 
 app.get('/download', (req,res) =>{
     res.download(path.resolve(__dirname,'static','index.html'))
---
+++ middlewares.js
@@ -0,0 +1,8 @@
+export function requestTime(req, res, next){
+    req.requestTime = Date.now()
+    next()
+}
+export function logger(req, res, next){
+    console.log(req.requestTime.toLocaleDateString())
+    next()
+}
