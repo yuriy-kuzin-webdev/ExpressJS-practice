cf126912f474241574d3b1852ca020082391132c
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-05 11:43:56 +0200

    added route middleware

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
-app.get('/', (req,res) =>{
-     res.sendFile(path.resolve(__dirname,'static','index.html'))
-})
+app.use(express.static(path.resolve(__dirname,'static')))
 
-app.get('/features', (req,res) =>{
-    res.sendFile(path.resolve(__dirname,'static','features.html'))
+app.get('/download', (req,res) =>{
+    res.download(path.resolve(__dirname,'static','index.html'))
 })
 
 app.listen(PORT, () => {
