eee46e23287993b3d1b908f041704cec4b8d5a99
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-07 16:10:59 +0200

    added controller

---
+++ controllers/servers.js
@@ -0,0 +1,3 @@
+export const getAll = (req, res) => {
+    res.status(200).json({test:"test"})
+}
--- routes/servers.js
+++ routes/servers.js
@@ -1,8 +1,7 @@
 import { Router } from 'express'
+import { getAll } from '../controllers/servers.js'
 const router = Router()
 
-router.get('/api/server', (req, res) => {
-    res.json({test:'test'})
-})
+router.get('/api/server', getAll)
 
 export default router
