bfcf3b70b3a342f757a87194525fe552d9bfd684
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-12-07 17:31:37 +0200

    added servers

--- controllers/servers.js
+++ controllers/servers.js
@@ -1,3 +1,10 @@
+let servers = [
+  { id: 1, name: "AWS", status: "working" },
+  { id: 2, name: "Azure", status: "working" },
+  { id: 3, name: "Render", status: "pending" },
+  { id: 4, name: "Heroku", status: "working" },
+];
+
 export const getAll = (req, res) => {
-    res.status(200).json({test:"test"})
-}
+  res.status(200).json(servers);
+};
--- static/app.js
+++ static/app.js
@@ -1,5 +1,13 @@
 const App = {
-    
+    data() {
+        return {
+            servers: []
+        }
+    },
+    async mounted() {
+        const res = await fetch('/api/server')
+        const servers = await res.json()
+    }
 }
 
 Vue.createApp(App).mount('#app')
